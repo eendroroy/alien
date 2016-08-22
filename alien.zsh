e5d97e4152e7d19c631ca57659d4a0f272b69507
indrajit <eendroroy@gmail.com>
2016-08-22 17:10:40 +0600

    show battery percent

--- alien.zsh
+++ alien.zsh
@@ -74,7 +74,7 @@ __ssh_client(){
 __battery_percent(){
   __bat_per=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{print $2}' | sed "s|%||g"`;
   if [[ -n $__bat_per ]]; then
-    echo " (B: ${__bat_per} )";
+    echo " (B: ${__bat_per})";
   fi
 }
 
