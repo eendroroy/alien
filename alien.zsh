44bb4772108289977864a0e517023cd653fd6346
indrajit <eendroroy@gmail.com>
2016-08-23 13:42:44 +0600

    add battery direction indicator

--- alien.zsh
+++ alien.zsh
@@ -77,6 +77,7 @@ __battery_stat(){
   if [[ $__bat_power = "charging" ]]; then __bat_power_ind="+ ";
   elif [[ $__bat_power = "discharging" ]]; then __bat_power_ind="- ";
   elif [[ $__bat_power = "fully-charged" ]]; then __bat_power_ind="◉ ";
+  fi
   __bat_per=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{print $2}' | sed "s|%||g"`;
   if [[ -n $__bat_per ]]; then
     echo " (B: ${__bat_power_ind}${__bat_per})";
