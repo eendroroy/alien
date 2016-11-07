22e6bea7105727cb862aa4142720d8cddfb24190
Indrajit Roy <indrajit@Indrajits-MacBook-Pro.local>
2016-11-08 02:46:45 +0600

    mac battery stat

--- alien.zsh
+++ alien.zsh
@@ -72,20 +72,29 @@ __ssh_client(){
 }
 
 __battery_stat(){
-  if which upower > /dev/null ; then
-    __bat_power=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | awk '{print $2}'`;
-    if [[ -n $__bat_power ]]; then
-      __bat_power=`pmset -g batt | tail -1 | awk '{print $4}' | tr -d "%;"`;
+  __os=`uname`;
+  if [ $__os == "Linux" ]; then
+    if which upower > /dev/null ; then
+      __bat_power=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | awk '{print $2}'`;
+      __bat_power_ind="";
+      if [[ $__bat_power = "charging" ]]; then __bat_power_ind="+";
+      elif [[ $__bat_power = "discharging" ]]; then __bat_power_ind="-";
+      elif [[ $__bat_power = "fully-charged" ]]; then __bat_power_ind="•";
+      fi
+      __bat_per=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{print $2}' | sed "s|%||g"`;
+      if [[ -n $__bat_per ]]; then
+        echo " | ${__bat_power_ind}${__bat_per}";
+      fi
     fi
+  fi
+  if [ $__os == "Darwin" ]
+    __bat_power=`pmset -g batt | tail -1 | awk '{print $4}' | tr -d "%;"`;
     __bat_power_ind="";
     if [[ $__bat_power = "charging" ]]; then __bat_power_ind="+";
     elif [[ $__bat_power = "discharging" ]]; then __bat_power_ind="-";
     elif [[ $__bat_power = "fully-charged" ]]; then __bat_power_ind="•";
     fi
-    __bat_per=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{print $2}' | sed "s|%||g"`;
-    if [[ -n $__bat_per ]]; then
-      __bat_per=`pmset -g batt | tail -1 | awk '{print $3}' | tr -d "%;"`
-    fi
+       __bat_per=`pmset -g batt | tail -1 | awk '{print $3}' | tr -d "%;"`
     if [[ -n $__bat_per ]]; then
       echo " | ${__bat_power_ind}${__bat_per}";
     fi
