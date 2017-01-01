6d46b74356cccdcf3cd84bda54550a26c22bcc8e
indrajit <eendroroy@gmail.com>
2017-01-02 03:34:12 +0600

    remove man page colors

--- alien.zsh
+++ alien.zsh
@@ -1,15 +1,5 @@
 #!/usr/bin/env zsh
 
-__color_man_page(){
-  export LESS_TERMCAP_mb=$(printf '\e[01;31m') # enter blinking mode - red
-  export LESS_TERMCAP_md=$(printf '\e[01;35m') # enter double-bright mode - bold, magenta
-  export LESS_TERMCAP_me=$(printf '\e[0m') # turn off all appearance modes (mb, md, so, us)
-  export LESS_TERMCAP_se=$(printf '\e[0m') # leave standout mode
-  export LESS_TERMCAP_so=$(printf '\e[01;33m') # enter standout mode - yellow
-  export LESS_TERMCAP_ue=$(printf '\e[0m') # leave underline mode
-  export LESS_TERMCAP_us=$(printf '\e[04;36m') # enter underline mode - cyan
-}
-
 _is_svn(){
   if [[ $(svn info 2>/dev/null) != "" ]]; then echo 1 ; else echo 0 ; fi
 }
