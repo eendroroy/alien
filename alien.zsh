e8e68bad343f5901a6b1e0690a4b3dfea8f9a353
indrajit <eendroroy@gmail.com>
2017-01-06 16:57:22 +0600

    add 256 native support

--- alien.zsh
+++ alien.zsh
@@ -1,5 +1,47 @@
 #!/usr/bin/env zsh
 
+_zsh_terminal_set_256color() {
+  if [[ "$TERM" =~ "-256color$" ]] ; then
+    [[ -n "${ZSH_256COLOR_DEBUG}" ]] && echo "zsh-256color: 256 color terminal already set." >&2
+    return
+  fi
+
+  local TERM256="${TERM}-256color"
+
+  # Use (n-)curses binaries, if installed.
+  if [[ -x "$( which toe )" ]] ; then
+    if toe -a | egrep -q "^$TERM256" ; then
+      _zsh_256color_debug "Found $TERM256 from (n-)curses binaries."
+      export TERM="$TERM256"
+      return
+    fi
+  fi
+
+  # Search through termcap descriptions, if binaries are not installed.
+  for termcaps in $TERMCAP "$HOME/.termcap" "/etc/termcap" "/etc/termcap.small" ; do
+    if [[ -e "$termcaps" ]] && egrep -q "(^$TERM256|\|$TERM256)\|" "$termcaps" ; then
+      _zsh_256color_debug "Found $TERM256 from $termcaps."
+      export TERM="$TERM256"
+      return
+    fi
+  done
+
+  # Search through terminfo descriptions, if binaries are not installed.
+  for terminfos in $TERMINFO "$HOME/.terminfo" "/etc/terminfo" "/lib/terminfo" "/usr/share/terminfo" ; do
+    if [[ -e "$terminfos"/$TERM[1]/"$TERM256" || \
+        -e "$terminfos"/"$TERM256" ]] ; then
+      _zsh_256color_debug "Found $TERM256 from $terminfos."
+      export TERM="$TERM256"
+      return
+    fi
+  done
+}
+
+_colorize(){
+  _zsh_terminal_set_256color
+  unset -f _zsh_terminal_set_256color
+}
+
 _is_svn(){
   if [[ $(svn info 2>/dev/null) != "" ]]; then echo 1 ; else echo 0 ; fi
 }
@@ -155,6 +197,7 @@ alien_prompts(){
   alien0
 }
 
+_colorize()
 autoload -U add-zsh-hook
 alien_prompts
 
