7ac5af4841df189388de5838fb6c69ca46ff732f
indrajit <eendroroy@gmail.com>
2017-05-25 22:51:07 +0600

    update structure

---
+++ modules/colors.zsh
@@ -0,0 +1,56 @@
+#!/usr/bin/env zsh
+
+
+if [[ "${ALIEN_THEME}" == "red" ]]; then
+  color0=088    # time bg
+  color1=226    # init bg
+  color1r=196   # init bg error
+  color2=254    # time fg
+  color3=202    # user bg
+  color4=232    # user fg
+  color5=214    # dir bg
+  color6=232    # dir fg
+  color7=238    # vcs bg
+  color8=228    # prompt fg
+  color9=226    # vcs fg
+elif [[ "${ALIEN_THEME}" == "green" ]]; then
+  color0=022    # time bg
+  color1=226    # init bg
+  color1r=196   # init bg error
+  color2=254    # time fg
+  color3=034    # user bg
+  color4=232    # user fg
+  color5=082    # dir bg
+  color6=232    # dir fg
+  color7=238    # vcs bg
+  color8=228    # prompt fg
+  color9=154    # vcs fg
+elif [[ "${ALIEN_THEME}" == "blue" ]]; then
+  color0=018    # time bg
+  color1=226    # init bg
+  color1r=196   # init bg error
+  color2=254    # time fg
+  color3=026    # user bg
+  color4=254    # user fg
+  color5=045    # dir bg
+  color6=019    # dir fg
+  color7=238    # vcs bg
+  color8=228    # prompt fg
+  color9=051    # vcs fg
+else
+  color0=018    # time bg
+  color1=226    # init bg
+  color1r=196   # init bg error
+  color2=254    # time fg
+  color3=026    # user bg
+  color4=254    # user fg
+  color5=045    # dir bg
+  color6=019    # dir fg
+  color7=238    # vcs bg
+  color8=228    # prompt fg
+  color9=051    # vcs fg
+fi
+color10=245     # lr bg
+color11=255     # lr fg
+color12=253     # dirty copy bg
+color13=016     # dirty copy fg
---
+++ modules/system.zsh
@@ -0,0 +1,54 @@
+#!/usr/bin/env zsh
+
+
+alien_storage_info(){
+  fs=`df -h . | tail -1 | awk '{print $1}' | sed "s|\.|•|g" `;
+  size=`df -h . | tail -1 | awk '{print $2}' | sed "s|\.|•|g" `;
+  used=`df -h . | tail -1 | awk '{print $3}' | sed "s|\.|•|g" `;
+  usedp=`df -h . | tail -1 | awk '{print $5}' | sed "s|\.|•|g" `;
+  free=`df -h . | tail -1 | awk '{print $4}' | sed "s|\.|•|g" `;
+  echo -n "💾 $fs - F:$free U:$used T:$size";
+}
+
+alien_date_time_info(){
+  echo -n "`date +%r`";
+}
+
+alien_ssh_client(){
+  if [ -n "${SSH_CLIENT}" ]; then
+    echo -n $SSH_CLIENT | awk {'print $1 " "'};
+  fi
+}
+
+alien_battery_stat(){
+  __os=`uname`;
+  if [[ $__os = "Linux" ]]; then
+    if which upower > /dev/null ; then
+      __bat_power=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | awk '{print $2}'`;
+      __bat_power_ind="";
+      if [[ $__bat_power = "charging" ]]; then __bat_power_ind="+";
+      elif [[ $__bat_power = "discharging" ]]; then __bat_power_ind="-";
+      elif [[ $__bat_power = "fully-charged" ]]; then __bat_power_ind="•";
+      fi
+      __bat_per=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{print $2}' | sed "s|%||g"`;
+      if [[ -n $__bat_per ]]; then
+        echo -n " | ${__bat_power_ind}${__bat_per}";
+      fi
+    fi
+  fi
+  if [[ $__os = "Darwin" ]]; then
+    __bat_power=`pmset -g batt | tail -1 | awk '{print $4}' | tr -d "%;"`;
+    __bat_power_ind="";
+    if [[ $__bat_power = "charging" ]]; then __bat_power_ind="+";
+    elif [[ $__bat_power = "discharging" ]]; then __bat_power_ind="-";
+    elif [[ $__bat_power = "finishing" ]]; then __bat_power_ind="+";
+    elif [[ $__bat_power = "charged" ]]; then __bat_power_ind="•";
+    fi
+       __bat_per=`pmset -g batt | tail -1 | awk '{print $3}' | tr -d "%;"`
+    if [[ -n $__bat_per ]]; then
+      echo -n " | ${__bat_power_ind}${__bat_per}";
+    fi
+  fi
+
+  unset __bat_power __bat_power_ind __bat_per
+}
