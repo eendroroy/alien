b68fc53db547ec50744aef9b1b54249d75359f44
indrajit <eendroroy@gmail.com>
2017-06-24 07:55:54 +0600

    update prompt

--- modules/async.zsh
+++ modules/async.zsh
@@ -5,7 +5,7 @@ function alien_dummy(){}
 function alien_lprompt_complete() {
   if [[ $(alien_is_git) == 1 ]]; then
     PROMPT='
-%(?.%K{$color0}%F{$color1}%f%k.%K{$color0}%F{$color1r}%f%k)%K{$color0}%F{$color2} $(alien_date_time_info)$(alien_battery_stat) %f%k%K{$color3}%F{$color0}%f%k%K{$color3}%F{$color4} $_user %f%k%K{$color5}%F{$color3}%f%k%K{$color5}%F{$color6} %3~ %f%k%F{$color5}%K{$color7}%k%f%K{$color7}%F{$color9}`alien_git_branch`%f%k%K{$color10}%F{$color7}%f%k%K{$color10}%F{$color11} $(alien_git_lr)%f%k%K{$color12}%F{$color10}%f%k%K{$color12}%F{$color13} $(alien_git_dirty)%f%k%F{$color12}%f
+%(?.%K{$color0}%F{$color1}%f%k.%K{$color0}%F{$color1r}%f%k)%K{$color0}%F{$color2} $(alien_date_time_info)$(alien_battery_stat) %f%k%K{$color3}%F{$color0}%f%k%K{$color3}%F{$color4} $_user %f%k%K{$color5}%F{$color3}%f%k%K{$color5}%F{$color6} %3~ %f%k%F{$color5}%K{$color7}%k%f%K{$color7}%F{$color9}`alien_git_branch`%f%k%K{$color10}%F{$color7}%f%k%K{$color10}%F{$color11}$(alien_git_lr) %f%k%K{$color12}%F{$color10}%f%k%K{$color12}%F{$color13}$(alien_git_dirty) %f%k%F{$color12}%f
 %F{$color3}$(alien_ssh_client)%f%F{$color8}%B❱%b%f '
   elif [[ $(alien_is_hg) == 1 ]]; then
     PROMPT='
