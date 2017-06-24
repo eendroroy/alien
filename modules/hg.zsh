b1a1c5d581219cd405227b03a536f58b0ced7eb3
indrajit <eendroroy@gmail.com>
2017-06-24 08:00:27 +0600

    update modules hg and svn

--- modules/hg.zsh
+++ modules/hg.zsh
@@ -1,11 +1,9 @@
 #!/usr/bin/env zsh
 
 alien_is_hg(){
-  if [[ $(hg branch 2>/dev/null) != "" ]]; then echo -n 1 ; else echo -n 0 ; fi
+  echo -ne `plib_is_hg`;
 }
 
 alien_hg_branch() {
-  ref=$(hg branch 2> /dev/null) || return true;
-  echo -n " H:  ${ref} ";
-  return true;
+  echo -n " H:  `plib_hg_branch`";
 }
