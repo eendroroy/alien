781a7cdc6e898e36e19fb7935ddca79e7927e9a1
indrajit <eendroroy@gmail.com>
2017-09-08 20:47:33 +0600

    fix venv

--- alien.zsh
+++ alien.zsh
@@ -28,6 +28,6 @@ function precmd(){
   
   PROMPT='
 %(?.%K{$color0}%F{$color1}%f%k.%K{$color0}%F{$color1r}%f%k)%K{$color0}%F{$color2} $(alien_date_time_info)$(alien_battery_stat) %f%k%K{$color3}%F{$color0}%f%k%K{$color3}%F{$color4} $_user %f%k%K{$color5}%F{$color3}%f%k%K{$color5}%F{$color6} %~ %f%k%F{$color5}%f
-%F{$color3}$(alien_ssh_client)%f%F{$color8}%F{$color14}`alien_venv` %f%B❱%b%f '
+%F{$color3}$(alien_ssh_client)%f%F{$color14}`alien_venv`%f%F{$color8}%B❱%b%f '
   alien_async_prompt
 }
