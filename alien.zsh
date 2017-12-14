0b1ee494bc05478adf108e93fe66c92f53a2b135
indrajit <eendroroy@gmail.com>
2017-12-15 00:24:44 +0600

    update theme

--- alien.zsh
+++ alien.zsh
@@ -2,14 +2,13 @@
 
 THEME_ROOT=${0:A:h}
 
+source "${THEME_ROOT}/modules/init.zsh"
+
 source "${THEME_ROOT}/libs/promptlib/activate"
 source "${THEME_ROOT}/libs/zsh-async/async.zsh"
 source "${THEME_ROOT}/libs/zsh-256color/zsh-256color.plugin.zsh"
 
-source "${THEME_ROOT}/modules/init.zsh"
-source "${THEME_ROOT}/modules/symbols.zsh"
 source "${THEME_ROOT}/modules/colors.zsh"
-
 source "${THEME_ROOT}/modules/system.zsh"
 source "${THEME_ROOT}/modules/git.zsh"
 source "${THEME_ROOT}/modules/hg.zsh"
