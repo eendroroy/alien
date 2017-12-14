c4377f103b7094df0d5edccd5e3cd5c68716b797
indrajit <eendroroy@gmail.com>
2017-12-15 00:21:55 +0600

    update init.zsh and symbols.zsh

--- modules/init.zsh
+++ modules/init.zsh
@@ -1,5 +1,22 @@
 #!/usr/bin/env zsh
 
-AM_GIT_SYM=G
-AM_HG_SYM=H
-AM_SVN_SYM=S
+[[ -z "${AM_GIT_SYM}" ]] && AM_GIT_SYM=G
+[[ -z "${AM_HG_SYM}" ]]  && AM_HG_SYM=H
+[[ -z "${AM_SVN_SYM}" ]] && AM_SVN_SYM=S
+
+[[ -z "${PLIB_GIT_PUSH_SYM}" ]] && PLIB_GIT_PUSH_SYM=↑
+[[ -z "${PLIB_GIT_PULL_SYM}" ]] && PLIB_GIT_PULL_SYM=↓
+
+if [[ "${ALIEN_THEME}" == "red" ]]; then
+  [[ -z "${PLIB_GIT_TRACKED_COLOR}" ]]   && PLIB_GIT_TRACKED_COLOR=124
+  [[ -z "${PLIB_GIT_UNTRACKED_COLOR}" ]] && PLIB_GIT_UNTRACKED_COLOR=236
+elif [[ "${ALIEN_THEME}" == "green" ]]; then
+  [[ -z "${PLIB_GIT_TRACKED_COLOR}" ]]   && PLIB_GIT_TRACKED_COLOR=64
+  [[ -z "${PLIB_GIT_UNTRACKED_COLOR}" ]] && PLIB_GIT_UNTRACKED_COLOR=236
+elif [[ "${ALIEN_THEME}" == "blue" ]]; then
+  [[ -z "${PLIB_GIT_TRACKED_COLOR}" ]]   && PLIB_GIT_TRACKED_COLOR=27
+  [[ -z "${PLIB_GIT_UNTRACKED_COLOR}" ]] && PLIB_GIT_UNTRACKED_COLOR=236
+else
+  [[ -z "${PLIB_GIT_TRACKED_COLOR}" ]]   && PLIB_GIT_TRACKED_COLOR=27
+  [[ -z "${PLIB_GIT_UNTRACKED_COLOR}" ]] && PLIB_GIT_UNTRACKED_COLOR=236
+fi
--- modules/symbols.zsh
+++ modules/symbols.zsh
@@ -1,12 +1,12 @@
 #!/usr/bin/env zsh
 
 if [[ $USE_NERD_FONT = 1 ]]; then
-  AM_GIT_SYM=
-  PLIB_GIT_ADD_SYM=
-  PLIB_GIT_DEL_SYM=
-  PLIB_GIT_MOD_SYM=
-  PLIB_GIT_NEW_SYM=
-  PLIB_GIT_PUSH_SYM=
-  PLIB_GIT_PULL_SYM=
-  AM_HG_SYM=
+  [[ -z "${AM_GIT_SYM}" ]]        && AM_GIT_SYM=
+  [[ -z "${PLIB_GIT_ADD_SYM}" ]]  && PLIB_GIT_ADD_SYM=
+  [[ -z "${PLIB_GIT_DEL_SYM}" ]]  && PLIB_GIT_DEL_SYM=
+  [[ -z "${PLIB_GIT_MOD_SYM}" ]]  && PLIB_GIT_MOD_SYM=
+  [[ -z "${PLIB_GIT_NEW_SYM}" ]]  && PLIB_GIT_NEW_SYM=
+  [[ -z "${PLIB_GIT_PUSH_SYM}" ]] && PLIB_GIT_PUSH_SYM=
+  [[ -z "${PLIB_GIT_PULL_SYM}" ]] && PLIB_GIT_PULL_SYM=
+  [[ -z "${AM_HG_SYM}" ]]         && AM_HG_SYM=
 fi
