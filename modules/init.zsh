e112faf9721b9b96a5d78bca5fbd595081dab580
indrajit <eendroroy@gmail.com>
2017-12-22 15:22:50 +0600

    rename AM_* to ALIEN_*

--- modules/init.zsh
+++ modules/init.zsh
@@ -1,19 +1,19 @@
 #!/usr/bin/env zsh
 
 if [[ $USE_NERD_FONT = 1 ]]; then
-  [[ -z "${AM_GIT_SYM}" ]]        && AM_GIT_SYM=
+  [[ -z "${ALIEN_GIT_SYM}" ]]     && ALIEN_GIT_SYM=
   [[ -z "${PLIB_GIT_ADD_SYM}" ]]  && PLIB_GIT_ADD_SYM=
   [[ -z "${PLIB_GIT_DEL_SYM}" ]]  && PLIB_GIT_DEL_SYM=
   [[ -z "${PLIB_GIT_MOD_SYM}" ]]  && PLIB_GIT_MOD_SYM=
   [[ -z "${PLIB_GIT_NEW_SYM}" ]]  && PLIB_GIT_NEW_SYM=
   [[ -z "${PLIB_GIT_PUSH_SYM}" ]] && PLIB_GIT_PUSH_SYM=
   [[ -z "${PLIB_GIT_PULL_SYM}" ]] && PLIB_GIT_PULL_SYM=
-  [[ -z "${AM_HG_SYM}" ]]         && AM_HG_SYM=
+  [[ -z "${ALIEN_HG_SYM}" ]]      && ALIEN_HG_SYM=
 fi
 
-[[ -z "${AM_GIT_SYM}" ]] && AM_GIT_SYM=G
-[[ -z "${AM_HG_SYM}" ]]  && AM_HG_SYM=H
-[[ -z "${AM_SVN_SYM}" ]] && AM_SVN_SYM=S
+[[ -z "${ALIEN_GIT_SYM}" ]] && ALIEN_GIT_SYM=G
+[[ -z "${ALIEN_HG_SYM}" ]]  && ALIEN_HG_SYM=H
+[[ -z "${ALIEN_SVN_SYM}" ]] && ALIEN_SVN_SYM=S
 
 [[ -z "${PLIB_GIT_PUSH_SYM}" ]] && PLIB_GIT_PUSH_SYM=↑
 [[ -z "${PLIB_GIT_PULL_SYM}" ]] && PLIB_GIT_PULL_SYM=↓
