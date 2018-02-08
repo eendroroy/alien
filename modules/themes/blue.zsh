69832dab02401bb87774ea5a84b1a4b5a5d93b4c
indrajit <eendroroy@gmail.com>
2018-02-08 14:30:58 +0600

    refactor

--- modules/themes/blue.zsh
+++ modules/themes/blue.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-alien_load_blue_theme(){
+alien_theme(){
   [[ -z $color0 ]]  && color0=018    # time bg
   [[ -z $color1 ]]  && color1=226    # init bg
   [[ -z $color1r ]] && color1r=196   # init bg error
