18b5796d07b794dc3280d2b051b02afa38ab6159
indrajit <eendroroy@gmail.com>
2018-02-08 14:27:35 +0600

    themes

---
+++ modules/themes/green.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+alien_load_green_theme(){
+  [[ -z $color0 ]]  && color0=022    # time bg
+  [[ -z $color1 ]]  && color1=226    # init bg
+  [[ -z $color1r ]] && color1r=196   # init bg error
+  [[ -z $color2 ]]  && color2=254    # time fg
+  [[ -z $color3 ]]  && color3=034    # user bg
+  [[ -z $color4 ]]  && color4=232    # user fg
+  [[ -z $color5 ]]  && color5=082    # dir bg
+  [[ -z $color6 ]]  && color6=232    # dir fg
+  [[ -z $color7 ]]  && color7=238    # vcs bg
+  [[ -z $color8 ]]  && color8=228    # prompt fg
+  [[ -z $color9 ]]  && color9=154    # vcs fg
+}
