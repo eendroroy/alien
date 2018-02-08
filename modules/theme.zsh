d06dcd8947822ab91f5a3a885bb6e223241da0d5
indrajit <eendroroy@gmail.com>
2018-02-08 14:47:16 +0600

    custom theme path

--- modules/theme.zsh
+++ modules/theme.zsh
@@ -3,10 +3,19 @@
 THEME_DIR="${0:A:h}/themes"
 
 alien_load_theme(){
-  if [[ -z "${ALIEN_THEME}" ]]; then
-    source "${THEME_DIR}/blue.zsh"
+  if [[ -z "${ALIEN_CUSTOM_THEME_PATH}" ]]; then
+    if [[ -z "${ALIEN_THEME}" ]]; then
+      source "${THEME_DIR}/blue.zsh"
+    else
+      source "${THEME_DIR}/${ALIEN_THEME}.zsh"
+    fi
   else
-    source "${THEME_DIR}/${ALIEN_THEME}.zsh"
+    if [[ -f "${ALIEN_CUSTOM_THEME_PATH}" ]]; then
+      source "${ALIEN_CUSTOM_THEME_PATH}"
+    else
+      echo "\n  --> (WARN) theme ${ALIEN_CUSTOM_THEME_PATH} not found. Loading default ..."
+      source "${THEME_DIR}/blue.zsh"
+    fi
   fi
   alien_theme
 
