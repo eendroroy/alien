1ebd886f2a120389efcc19abbf474e0066c53001
betaboon <betaboon@0x80.ninja>
2019-02-24 03:34:59 +0100

    section vcs: cleanup

--- modules/async.zsh
+++ modules/async.zsh
@@ -5,13 +5,20 @@ function alien_dummy(){}
 function alien_lprompt_complete() {
   alien_prompt_start
   if [[ $(alien_is_git) == 1 ]]; then
-    alien_prompt_append_section "$(alien_git_branch)" $color9 $color7 $ALIEN_SECTION_SEP_SYM
-    alien_prompt_append_section "$(alien_git_stash)$(alien_git_lr) " $color10 $color12 $ALIEN_SECTION_SEP_SYM
-    alien_prompt_append_section "$(alien_git_dirty) " $color13 $color12 $ALIEN_SECTION_SEP_SYM
+    alien_prompt_append_section " $(alien_git_branch) " $color9 $color7
+    __git_stash=$(alien_git_stash)
+    __git_lr=$(alien_git_lr)
+    __git_dirty=$(alien_git_dirty)
+    if [[ -n "$__git_stash" ]] || [[ -n "$__git_lr" ]]; then
+      alien_prompt_append_section "${__git_stash}${__git_lr}" $color9 $color7 $ALIEN_SECTION_SEP_SYM
+    fi
+    if [[ -n "$__git_dirty" ]]; then
+      alien_prompt_append_section "${__git_dirty}" $color9 $color7 $ALIEN_SECTION_SEP_SYM
+    fi
   elif [[ $(alien_is_hg) == 1 ]]; then
-    alien_prompt_append_section "$(alien_hg_branch)" $color7 $color10 $ALIEN_SECTION_SEP_SYM
+    alien_prompt_append_section "$(alien_hg_branch)" $color9 $color7 $ALIEN_SECTION_SEP_SYM
   elif [[ $(alien_is_svn) == 1 ]]; then
-    alien_prompt_append_section "$(alien_svn_branch)" $color7 $color10 $ALIEN_SECTION_SEP_SYM
+    alien_prompt_append_section "$(alien_svn_branch)" $color9 $color7 $ALIEN_SECTION_SEP_SYM
   fi
   alien_prompt_end
   PROMPT=$(alien_prompt_render)
