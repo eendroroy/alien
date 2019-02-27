0dff5fd2353df588bf82e1e3b6517fcbc8d8d6e0
betaboon <betaboon@0x80.ninja>
2019-02-27 21:15:21 +0100

    prompt-renderer: even further refactoring, allowing reorder etc

--- alien.zsh
+++ alien.zsh
@@ -9,6 +9,7 @@ source "${THEME_ROOT}/libs/zsh-async/async.zsh"
 source "${THEME_ROOT}/libs/zsh-256color/zsh-256color.plugin.zsh"
 
 source "${THEME_ROOT}/modules/theme.zsh"
+source "${THEME_ROOT}/modules/sections.zsh"
 source "${THEME_ROOT}/modules/prompt.zsh"
 source "${THEME_ROOT}/modules/system.zsh"
 source "${THEME_ROOT}/modules/git.zsh"
@@ -24,8 +25,6 @@ function precmd(){
 
   RPROMPT=''
   if [[ ${PROMPT} == "" ]] || [[ ${ALIEN_KEEP_PROMPT} != 1 ]]; then
-    alien_prompt_start
-    alien_prompt_end
     PROMPT=$(alien_prompt_render)
   fi
   alien_async_prompt
--- modules/async.zsh
+++ modules/async.zsh
@@ -3,36 +3,10 @@
 function alien_dummy(){}
 
 function alien_lprompt_complete() {
-  alien_prompt_start
-  if [[ $ALIEN_SECTION_VCS_ENABLE != 0 ]]; then
-    if [[ $(alien_is_git) == 1 ]]; then
-      alien_prompt_append_section " $(alien_git_branch) " \
-        $ALIEN_SECTION_VCS_BRANCH_FG $ALIEN_SECTION_VCS_BRANCH_BG $ALIEN_SECTION_SEP_SYM
-      __git_stash=$(alien_git_stash)
-      __git_lr=$(alien_git_lr)
-      __git_dirty=$(alien_git_dirty)
-      if [[ -n "$__git_stash" ]] || [[ -n "$__git_lr" ]]; then
-        alien_prompt_append_section "${__git_stash}${__git_lr}" \
-          $ALIEN_SECTION_VCS_STATUS_FG $ALIEN_SECTION_VCS_STATUS_BG $ALIEN_SECTION_SEP_SYM
-      fi
-      if [[ -n "$__git_dirty" ]]; then
-        alien_prompt_append_section "${__git_dirty}" \
-          $ALIEN_SECTION_VCS_DIRTY_FG $ALIEN_SECTION_VCS_DIRTY_BG $ALIEN_SECTION_SEP_SYM
-      fi
-    elif [[ $(alien_is_hg) == 1 ]]; then
-      alien_prompt_append_section "$(alien_hg_branch)" \
-        $ALIEN_SECTION_VCS_BRANCH_FG $ALIEN_SECTION_VCS_BRANCH_FG $ALIEN_SECTION_SEP_SYM
-    elif [[ $(alien_is_svn) == 1 ]]; then
-      alien_prompt_append_section "$(alien_svn_branch)" \
-        $ALIEN_SECTION_VCS_BRANCH_FG $ALIEN_SECTION_VCS_BRANCH_FG $ALIEN_SECTION_SEP_SYM
-    fi
-  fi
-  alien_prompt_end
-  PROMPT=$(alien_prompt_render)
+  PROMPT=$(alien_prompt_render 'async')
   zle && zle reset-prompt
   async_stop_worker lprompt -n
 }
-
 
 alien_async_prompt(){
   async_init
